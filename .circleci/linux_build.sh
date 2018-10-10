5baaa5a8efa69c3b54a983b9a1feb3820b6ec99f
Pahaz White <pahaz.white@gmail.com>
2018-10-11 00:30:18 +0300

    install virtualenv

--- .circleci/linux_build.sh
+++ .circleci/linux_build.sh
@@ -5,7 +5,7 @@ set -ex
 rm -rf venv
 
 uname -a
-virtualenv -p ${TOOLCHAIN_PATH}/bin/python3 venv
+virtualenv venv
 venv/bin/pip install pyinstaller==3.3.1
 venv/bin/pyinstaller py3line.py --onefile
 mv dist/py3line dist/py3line-Linux-x86_64
